8def3f24ba643a5ee428585e159e6726ad5409c7
szal-balazs <szal.balazs@gmail.com>
2023-07-18 15:15:54 +0200

    Interactions between player and sample lines

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
 import random
 
-tile_types = {"yellow": 20,
-              "red": 20,
-              "blue": 20,
-              "white": 20,
-              "black": 20
-              }
-
 
 class Manufacturer:
 
@@ -30,59 +23,130 @@ class Player:
 
     def __init__(self):
         self.hand = []
-        self.floor = []
+        self.base_line = []
         self.first_sample_line = []
         self.second_sample_line = []
         self.third_sample_line = []
         self.fourth_sample_line = []
         self.fifth_sample_line = []
 
-    def take_from_man(self, manufacturer):
+    @staticmethod
+    def choose_man():
+        man_answer = ""
+        while man_answer not in range(1, 5):
+            try:
+                man_answer = int(input("Which manufacturer you wanna take from ?\n"
+                                       "[1,2,3,4,5]\n"))
+            except ValueError:
+                print("Please enter a whole number.")
+            else:
+                return man_answer
+
+    def take_from_man(self, manufacturer, table):
         answer = input("Which color would you like to take ?\n")
         while answer not in manufacturer.offer:
             answer = input("Please enter a color that the manufacturer has !\n")
         for i in range(manufacturer.offer.count(answer)):
             manufacturer.offer.remove(answer)
             self.hand.append(answer)
+        for i in range(len(manufacturer.offer)):
+            table.append(manufacturer.offer[0])
+            del manufacturer.offer[0]
 
     def put_in_sample_line(self):
-        sample_line = {
+        sample_lines = {
             "first": self.first_sample_line,
             "second": self.second_sample_line,
             "third": self.third_sample_line,
             "fourth": self.fourth_sample_line,
             "fifth": self.fifth_sample_line,
         }
-        answer = input("Which sample line you want to put your tile ?\n")
-        while answer not in sample_line.keys():
-            answer = input("Please enter a valid line !\n")
-        while len(sample_line[answer]) >= list(sample_line.keys()).index(answer) + 1:
-            answer = input("You cant put down any more tile in that line !\n")
-        color = input("Which color would you like to put down ?\n")
-        while color not in self.hand:
-            color = input("Please enter a color that you have !\n")
 
-        tile_counter = 0
-        while len(sample_line[answer]) < list(sample_line.keys()).index(answer) + 1 \
-                and color in self.hand:
-            self.hand.remove(color)
-            sample_line[answer].append(color)
-            tile_counter += 1
+        def check_sample_lines(a_list):
+            for key, value in sample_lines.items():
+                if len(value) < list(sample_lines.keys()).index(key) + 1:
+                    if not value or color in value:
+                        a_list.append(key)
 
-        print(f"You put down {tile_counter} {color} tile to the {answer} line")
+        color = self.hand[0]
+
+        possible_places = ("sample", "base")
+        possible_sample_lines = []
+        check_sample_lines(possible_sample_lines)
+
+        while possible_sample_lines:
+            while self.hand:
+                print(f" Your hand : {self.hand}")
+                line_answer = input("Which line you want to put your tile ?\n"
+                                    f"{possible_places}\n")
+                while line_answer not in possible_places:
+                    line_answer = input("Please enter a valid line !\n")
+                if line_answer == "sample":
+                    sample_answer = input("Which line you want to put your tile ?\n"
+                                          f"{possible_sample_lines}\n")
+                    while sample_answer not in possible_sample_lines:
+                        sample_answer = input("Please enter a valid sample line !\n")
+
+                    self.hand.remove(color)
+                    sample_lines[sample_answer].append(color)
+
+                    print(f"You put down a {color} tile to the {sample_answer} line")
+
+                    possible_sample_lines = []
+                    check_sample_lines(possible_sample_lines)
+            else:
+                break
+        else:
+            print("You have to put your remaining tile(s) to the base line. ")
+            while not self.hand:
+                self.base_line.append(self.hand[0])
+                del self.hand[0]
 
 
-manufacturer1 = Manufacturer()
-manufacturer1.make_offer(tile_types)
+def main():
+    bag = {"yellow": 20,
+           "red": 20,
+           "blue": 20,
+           "white": 20,
+           "black": 20
+           }
 
-print(f" Offer : {manufacturer1}")
+    table = []
 
-player = Player()
-player.take_from_man(manufacturer1)
+    player = Player()
 
-print(tile_types)
-print(f" Offer : {manufacturer1}")
-print(f" Hand : {player.hand}")
+    manufacturer1 = Manufacturer()
+    manufacturer2 = Manufacturer()
+    manufacturer3 = Manufacturer()
+    manufacturer4 = Manufacturer()
+    manufacturer5 = Manufacturer()
 
-player.put_in_sample_line()
-print(player.hand)
+    manufacturer1.make_offer(bag)
+    manufacturer2.make_offer(bag)
+    manufacturer3.make_offer(bag)
+    manufacturer4.make_offer(bag)
+    manufacturer5.make_offer(bag)
+
+    print(f" Man. 1 offer : {manufacturer1}")
+    print(f" Man. 2 offer : {manufacturer2}")
+    print(f" Man. 3 offer : {manufacturer3}")
+    print(f" Man. 4 offer : {manufacturer4}")
+    print(f" Man. 5 offer : {manufacturer5}")
+
+    man_table = {
+        1: manufacturer1,
+        2: manufacturer2,
+        3: manufacturer3,
+        4: manufacturer4,
+        5: manufacturer5
+    }
+
+    chosen_man = man_table[player.choose_man()]
+    player.take_from_man(chosen_man, table)
+    print(f" Hand : {player.hand}")
+
+    player.put_in_sample_line()
+
+
+if __name__ == "__main__":
+    main()
